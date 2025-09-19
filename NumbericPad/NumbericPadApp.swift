db56e71886299747a558e45d4429efda33b27677
Drbingbing <tw718140@gmail.com>
2025-09-19 10:14:00 +0800

    Initial Commit

---
+++ NumbericPad/NumbericPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumbericPadApp.swift
+//  NumbericPad
+//
+//  Created by BingBing on 2025/9/19.
+//
+
+import SwiftUI
+
+@main
+struct NumbericPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
